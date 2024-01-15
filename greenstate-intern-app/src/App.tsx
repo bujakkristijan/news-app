d8650c152dc9c60817938a843356204c24f01d82
Kristijan Bujak <kbujak996@gmail.com>
2024-01-15 17:45:27 +0100

    Theme added, tested for Text styled component

--- greenstate-intern-app/src/App.tsx
+++ greenstate-intern-app/src/App.tsx
@@ -1,14 +1,24 @@
 import './App.css'
 import HeaderComponent from './components/header/HeaderComponent'
 import GlobalStyles from './global-styles/GlobalStyles'
+import styled from 'styled-components';
+import Theme from './shared/theme/Theme';
+
+const Text = styled.div`
+  color: ${(props)=> props.theme.colors.header};
+  font-size: ${(props)=> props.theme.fontSizesHeader.large};
+`;
 
 function App() {
   
 
   return (
     <>
-      <GlobalStyles/>
-      <HeaderComponent/>
+    <Theme>
+        <GlobalStyles/>
+        <HeaderComponent/>
+        <Text>Test text</Text>
+    </Theme>
     </>
   )
 }
---
+++ greenstate-intern-app/src/shared/theme/Theme.tsx
@@ -0,0 +1,27 @@
+import { ThemeProvider } from "styled-components";
+const defaultTheme = {
+    colors: {
+        header: "red"
+    },
+    fonts:{
+        header: "Montserrat",
+        body: "Inter"
+
+    },
+    fontSizesHeader: {
+        large: "3rem",
+        regular: "2rem",
+        small: "1.5rem"
+    },
+    fontWeightsHeader: {
+        bold: "700",
+        semiBold: "600",
+        regular: "400"  
+    }
+        
+  };
+
+  export default function Theme({children}: any){
+    return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+  }
+
