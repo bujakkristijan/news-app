6f6efd9028d2063b4c4dd51c1a7fa70cd2e60a1f
Kristijan Bujak <kbujak996@gmail.com>
2024-01-16 09:25:02 +0100

    removed styled import from App.tsx

--- greenstate-intern-app/src/App.tsx
+++ greenstate-intern-app/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import HeaderComponent from './components/header/HeaderComponent'
 import GlobalStyles from './global-styles/GlobalStyles'
-import styled from 'styled-components';
 import ThemeProvider from './shared/theme/ThemeProvider';
 
 function App() {
