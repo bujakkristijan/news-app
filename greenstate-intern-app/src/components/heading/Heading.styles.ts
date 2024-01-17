cbae4e57cf3b67f8583da58d2edfedf1c009cfaa
Kristijan Bujak <kbujak996@gmail.com>
2024-01-17 10:24:03 +0100

    Prevent generating HTML attributes from props

--- greenstate-intern-app/src/components/heading/Heading.styles.ts
+++ greenstate-intern-app/src/components/heading/Heading.styles.ts
@@ -2,14 +2,14 @@ import styled from "styled-components";
 import { TypographyHeadingType, TypographyFontWeight, TypographyFontFamily } from "../../shared/theme/theme";
 
 export const StyledHeading = styled.p.attrs<{
-  headingType: TypographyHeadingType,
-  fontWeight: TypographyFontWeight,
-  fontFamily: TypographyFontFamily;
-}>(({headingType}) =>({
-    as: headingType,
+  $headingType: TypographyHeadingType,
+  $fontWeight: TypographyFontWeight,
+  $fontFamily: TypographyFontFamily;
+}>(({$headingType}) =>({
+    as: $headingType,
 }))`
-  font-size: ${({ theme, headingType }) => theme.typography.headingType[headingType].fontSize};
-  font-weight: ${({ theme, fontWeight }) => theme.typography.fontWeight[fontWeight]};
-  font-family: ${({ theme, fontFamily }) => theme.typography.fontFamily[fontFamily]};
-  line-height: ${({ theme, headingType }) => theme.typography.headingType[headingType].lineHeight};
+  font-size: ${({ theme, $headingType }) => theme.typography.headingType[$headingType].fontSize};
+  font-weight: ${({ theme, $fontWeight }) => theme.typography.fontWeight[$fontWeight]};
+  font-family: ${({ theme, $fontFamily }) => theme.typography.fontFamily[$fontFamily]};
+  line-height: ${({ theme, $headingType }) => theme.typography.headingType[$headingType].lineHeight};
 `;
