0e89961bf6f6b7a321ff26961cf674b0caba8855
Kristijan Bujak <kbujak996@gmail.com>
2024-01-18 10:28:07 +0100

    IconWrapper updated

--- greenstate-intern-app/src/App.tsx
+++ greenstate-intern-app/src/App.tsx
@@ -4,9 +4,10 @@ import ThemeProvider from './shared/theme/ThemeProvider';
 import { Text } from './components/text/Text';
 import { Heading } from './components/heading/Heading';
 import { Chip } from './components/chip/Chip';
-import { SuccessIcon } from './components/icon/icons/SuccessIcon';
 import { IconWrapper } from './components/icon/icon-wrapper/IconWrapper';
-import { ErrorIcon } from './components/icon/icons/ErrorIcon';
+import { HomeIcon } from './components/icon/icons/HomeIcon';
+import { NewPostIcon } from './components/icon/icons/NewPostIcon';
+import { AllNewsIcon } from './components/icon/icons/AllNewsIcon';
 
 function App() {
 
@@ -18,8 +19,9 @@ function App() {
         <Heading headingType='h1' fontWeight='bold' fontFamily='montserrat'>TEST HEADING1</Heading>
         <Chip isActive={true} >23/12/2023</Chip>
         <Chip isActive={false} >23/12/2023</Chip>
-        <IconWrapper icon={<SuccessIcon/>} width="100px" color="green" height="100px"></IconWrapper>
-        <IconWrapper icon={<ErrorIcon/>} width="150px" color="red" height="150px"></IconWrapper>
+        <IconWrapper icon={HomeIcon} size ="150px" color="red"></IconWrapper>
+        <IconWrapper icon={NewPostIcon} size ="80px" color="blue"></IconWrapper>
+        <IconWrapper icon={AllNewsIcon} size ="40px" color="green"></IconWrapper>
     </ThemeProvider>
     </>
   )
--- greenstate-intern-app/src/components/icon/icon-wrapper/IconWrapper.tsx
+++ greenstate-intern-app/src/components/icon/icon-wrapper/IconWrapper.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
 import { StyledIcon } from './StyledIcon';
 
-type IconWrapper = {
-    icon: React.ReactNode;
-    width: string;
-    height: string;
-    color: string;
-  }
+type IconWrapperProps = {
+  icon: React.ComponentType<{ color: string }>;
+  size: string;
+  color: string;
+};
 
-export const IconWrapper = ({ icon, width, height, color} : IconWrapper) => {
-    return (
-      <StyledIcon width={width} height={height} color={color}>
-        {icon}
-      </StyledIcon>
-    );
-  };
+export const IconWrapper = ({ icon: IconComponent, size, color } :IconWrapperProps) => {
+  return (
+    <StyledIcon size ={size}>
+      <IconComponent color={color} />
+    </StyledIcon>
+  );
+};
+
+export default IconWrapper;
