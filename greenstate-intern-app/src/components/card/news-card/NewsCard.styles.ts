9d9b72e1513d0ee1acb22b37c62c5bf6ac33c785
Kristijan Bujak <kbujak996@gmail.com>
2024-01-24 15:22:37 +0100

    Max-width changed for NewsCard

--- greenstate-intern-app/src/components/card/news-card/NewsCard.styles.ts
+++ greenstate-intern-app/src/components/card/news-card/NewsCard.styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledNewsCard = styled.div`
-  max-width: 18.75rem;
+  max-width: 16.75rem;
   position: relative;
   display: flex;
   flex-direction: column;
