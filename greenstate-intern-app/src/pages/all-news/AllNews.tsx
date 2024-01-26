5e33cad34785f63c7162fd5655ca740a6e8a9efe
Kristijan Bujak <kbujak996@gmail.com>
2024-01-26 13:21:28 +0100

    Router added and tested

---
+++ greenstate-intern-app/src/pages/all-news/AllNews.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AllNews = () => {
+  return <div>AllNews</div>;
+};
---
+++ greenstate-intern-app/src/router/routes/routes.ts
@@ -0,0 +1,12 @@
+
+export type Route = {
+    path: string;
+    element: React.ReactElement;
+  };
+
+export const routes ={
+    home:'/home',
+    newPost:'/new-post',
+    allNews: '/all-news'
+    
+}
