2a0863e4eafed397ae89cb0926672b103a5a5058
Kristijan Bujak <kbujak996@gmail.com>
2024-01-30 10:42:20 +0100

    Pageshell update

--- greenstate-intern-app/src/components/page-shell/PageShell.styles.ts
+++ greenstate-intern-app/src/components/page-shell/PageShell.styles.ts
@@ -2,7 +2,9 @@ import styled from "styled-components"
 export const StyledPageShell = styled.div`
     max-width: 77.5rem;
     width: 100%;
-    padding-left: 1.25rem;
-    padding-right: 1.25rem;
     margin: auto;
+    @media(max-width: 1280px){
+        padding-left: 1.25rem;
+        padding-right: 1.25rem;
+    }
 `
