5b53fa2ef169fecda3e5a81d57c00659fa8ac278
Kristijan Bujak <kbujak996@gmail.com>
2024-02-08 09:46:33 +0100

    Card props changed

--- greenstate-intern-app/src/components/card/Card.tsx
+++ greenstate-intern-app/src/components/card/Card.tsx
@@ -7,31 +7,31 @@ import {
 } from "../../shared/theme/theme";
 type CardProps = {
   children: React.ReactNode;
-  $borderwidth?: TypographyBorderWidth;
-  $borderRadius?: TypographyBorderRadius;
-  $bgColor?: Color;
-  $padding?: Padding;
-  $borderColor?: Color;
-  $width?: string;
+  borderwidth?: TypographyBorderWidth;
+  borderRadius?: TypographyBorderRadius;
+  bgColor?: Color;
+  padding?: Padding;
+  borderColor?: Color;
+  width?: string;
 };
 
 export const Card = ({
   children,
-  $borderwidth = "sm",
-  $borderRadius = "md",
-  $bgColor = "white",
-  $padding = "1rem",
-  $borderColor = "lightestGrey",
-  $width = "fit-content",
+  borderwidth = "sm",
+  borderRadius = "md",
+  bgColor = "white",
+  padding = "1rem",
+  borderColor = "lightestGrey",
+  width = "fit-content",
 }: CardProps) => {
   return (
     <StyledCard
-      $borderWidth={$borderwidth}
-      $borderRadius={$borderRadius}
-      $bgColor={$bgColor}
-      $padding={$padding}
-      $borderColor={$borderColor}
-      $width={$width}
+      $borderWidth={borderwidth}
+      $borderRadius={borderRadius}
+      $bgColor={bgColor}
+      $padding={padding}
+      $borderColor={borderColor}
+      $width={width}
     >
       {children}
     </StyledCard>
