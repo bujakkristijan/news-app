e9291ab44858c5b05f4ea1d06648ca339d2d7c23
Kristijan Bujak <kbujak996@gmail.com>
2024-02-10 16:26:32 +0100

    Small change

--- greenstate-intern-app/src/pages/not-found/NotFound.styles.ts
+++ greenstate-intern-app/src/pages/not-found/NotFound.styles.ts
@@ -1,10 +1,7 @@
 import styled from "styled-components";
 export const NotFoundWrapper = styled.div`
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  position: fixed;
+  min-height: 100vh;
+  width: 100vw;
   background-color: white;
   display: flex;
   align-items: center;
