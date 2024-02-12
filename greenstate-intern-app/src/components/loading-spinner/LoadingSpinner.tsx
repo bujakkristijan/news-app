0d5bf977a6ca7188c68b809b6126852b5f65305d
Kristijan Bujak <kbujak996@gmail.com>
2024-02-12 09:34:07 +0100

    Puff loading spinner added, deleted node_modules and other files outside of project

--- greenstate-intern-app/src/components/loading-spinner/LoadingSpinner.tsx
+++ greenstate-intern-app/src/components/loading-spinner/LoadingSpinner.tsx
@@ -1,10 +1,16 @@
-import { StyledSpinner } from "./LoadingSpinner.styles";
+import { Puff } from "react-loader-spinner";
 import { StyledSpinnerWrapper } from "./LoadingSpinner.styles";
 
 export const LoadingSpinner = () => {
   return (
     <StyledSpinnerWrapper>
-      <StyledSpinner />
+      <Puff
+        visible={true}
+        height="64"
+        width="64"
+        color="#3381ff"
+        ariaLabel="puff-loading"
+      />
     </StyledSpinnerWrapper>
   );
 };
